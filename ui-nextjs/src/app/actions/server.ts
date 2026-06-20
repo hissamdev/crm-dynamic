53032b07abdbad1f5c3db9df112c4e0bbf4b742d
Hissam <hissamdev@gmail.com>
2026-06-20 16:41:57 +0600

    Finalized server action for creating new list with zod validation

--- ui-nextjs/src/app/actions/server.ts
+++ ui-nextjs/src/app/actions/server.ts
@@ -2,6 +2,7 @@
 
 import prisma from "@/src/lib/prisma";
 import { Field } from "../dashboard/create-list/page";
+import z from "zod";
 
 export type List = {
     name: FormDataEntryValue | null;
@@ -9,16 +10,64 @@ export type List = {
     desc: FormDataEntryValue | null;
 };
 
+const listSchema = z.object({
+    name: z
+        .string()
+        .min(2, "List name should be at least 2 characters")
+        .max(120, "List name is too long"),
+    emoji: z.string().max(20, "Emoji is too long"),
+    desc: z.string().min(2, "List description should be at least 2 characters"),
+});
+
+const fieldSchema = z.array(
+    z.object({
+        name: z
+            .string()
+            .min(1, "Field name should be at least 1 character")
+            .max(250, "Field name should be within 250 characters"),
+        emoji: z.string().max(20, "Emoji is too long"),
+        type: z.string().max(40, "Type is too long"),
+    }),
+);
+
 export async function handleCreateList(fields: Field[], formData: FormData) {
     const listInfo = {
         name: formData.get("list-name"),
         emoji: formData.get("list-emoji"),
         desc: formData.get("list-desc"),
     };
-    console.log(
-        "List info:",
-        JSON.stringify(listInfo, null, 2),
-        "\n Fields:",
-        fields,
-    );
+
+    // Validation
+    const listResult = listSchema.safeParse(listInfo);
+    if (!listResult.success) {
+        console.error(listResult.error.issues);
+        return;
+    }
+    const fieldResult = fieldSchema.safeParse(fields);
+    if (!fieldResult.success) {
+        console.error(fieldResult.error.issues);
+        return;
+    }
+
+    const preparedFields = fieldResult.data.map((field, idx) => ({
+        name: field.name,
+        emoji: field.emoji,
+        type: field.type,
+        label: field.name.toLocaleLowerCase(),
+        position: idx + 1,
+    }));
+
+    try {
+        await prisma.list.create({
+            data: {
+                name: listResult.data.name,
+                desc: listResult.data.desc,
+                fields: {
+                    create: preparedFields,
+                },
+            },
+        });
+    } catch (e) {
+        console.error("Failed to create list:", e);
+    }
 }
