d34bcb3caa2f1062a8f5a23ebca07c6299921064
Hissam <hissamdev@gmail.com>
2026-06-27 13:53:07 +0600

    Added shadcn

---
+++ ui-nextjs/src/proxy.ts
@@ -0,0 +1,18 @@
+import { headers } from "next/headers";
+import { auth } from "./lib/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function proxy(req: NextRequest) {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+    if (!session) {
+        return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/dashboard:path*"],
+};
