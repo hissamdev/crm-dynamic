ca61576b17b0659a6e445048a1d6d0de7b805842
Hissam <hissamdev@gmail.com>
2026-06-28 15:14:50 +0600

    wip: Remade the form without shadcn components

--- ui-nextjs/src/app/auth/sign-in/page.tsx
+++ ui-nextjs/src/app/auth/sign-in/page.tsx
@@ -9,49 +9,45 @@ import { Input } from "@/components/ui/input";
 
 export default function SignIn() {
     return (
-        <main className="h-screen bg-black font-inter">
-            <div className="mx-auto mt-40 w-fit">
-                <form>
-                    <FieldGroup className="py-14 px-10 min-w-143.75 min-h-100 border border-gray-500 rounded-2xl">
-                        <Field>
-                            <FieldLabel
-                                htmlFor="email"
-                                className="text-xl font-semibold text-white"
-                            >
-                                Enter your email
-                            </FieldLabel>
-                            <Input
-                                id="email"
-                                placeholder="e.g. your-email@provider.com"
-                                className="text-white py-5 border-gray-400"
-                            />
-                            <FieldDescription className="mt-2 block text-white text-lg font-light">
-                                Type in your email to receive a code for signing
-                                in
-                            </FieldDescription>
-                        </Field>
-                        <Field className="">
-                            <div className="flex items-center gap-4">
-                                <Checkbox id="terms" />
-                                <FieldLabel
-                                    htmlFor="terms"
-                                    className="text-white"
-                                >
-                                    I acknowledge that this application is a
-                                    work in progress
-                                </FieldLabel>
-                            </div>
-                        </Field>
-                        <Field className="mt-12">
-                            <div>
-                                <button className="block mx-auto px-8 py-1.5 bg-white rounded-md cursor-pointer">
-                                    Submit
-                                </button>
-                            </div>
-                        </Field>
-                    </FieldGroup>
-                </form>
-            </div>
+        <main className="h-screen bg-black font-inter text-white">
+            <form className="mx-auto max-w-100 py-10 px-8 min-h-90 border border-gray-500 rounded-2xl">
+                <fieldset>
+                    <label htmlFor="email" className="text-lg">
+                        Enter your email
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="email@provider.com"
+                        className="mt-2 px-3 py-2 w-full border border-gray-700 rounded-md text-sm"
+                    />
+                    <p className="mt-2 text-sm font-light leading-tight">
+                        An email containin a link will be sent to your inbox
+                    </p>
+                </fieldset>
+
+                <fieldset className="mt-10 flex items-start gap-4">
+                    <input
+                        type="checkbox"
+                        id="checkbox"
+                        name="checkbox"
+                        className="
+                        mt-1
+                        
+                            cursor-pointer
+                        "
+                    />
+                    <label htmlFor="checkbox" className="text-sm">
+                        I acknowledge that this project is a work in progress
+                        and is being displayed for demonstration purposes.
+                    </label>
+                </fieldset>
+
+                <button className="block mx-auto mt-12 px-4 py-1.5 bg-white hover:bg-white/80 text-black cursor-pointer rounded-md">
+                    Submit
+                </button>
+            </form>
         </main>
     );
 }
