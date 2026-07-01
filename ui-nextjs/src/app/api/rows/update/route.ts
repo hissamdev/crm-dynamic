f2b9b5deb0b08fcf085420cd4838aede1db0b605
Hissam <hissamdev@gmail.com>
2026-07-01 11:04:16 +0600

    Fixed auth flow in api/rows/update

--- ui-nextjs/src/app/api/rows/update/route.ts
+++ ui-nextjs/src/app/api/rows/update/route.ts
@@ -1,4 +1,6 @@
+import { auth } from "@/src/lib/auth";
 import prisma from "@/src/lib/prisma";
+import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import z from "zod";
 
@@ -8,10 +10,25 @@ const schema = z.object({
     data: z.record(z.string(), z.string()),
 });
 
+type ReqBody = {
+    listId: string;
+    rowId: number;
+    data: Record<string, string>;
+};
+
 export async function PUT(req: NextRequest) {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+    if (!session) {
+        return NextResponse.json({
+            success: false,
+            message: "Unauthorized",
+        });
+    }
     // id, listId, data
     const body = await req.json();
-    const { listId, rowId, data } = body;
+    const { listId, rowId, data }: ReqBody = body;
     if (!listId || !rowId || !data) {
         console.error("Invalid request body");
         return NextResponse.json(
@@ -36,6 +53,21 @@ export async function PUT(req: NextRequest) {
     }
 
     try {
+        // Check if user has access to the row being updated
+        const isUserOwner = await prisma.list.findFirst({
+            where: {
+                id: listId,
+                userId: session.user.id,
+            },
+        });
+
+        if (!isUserOwner) {
+            return NextResponse.json({
+                success: false,
+                message: "No access to specified list",
+            });
+        }
+
         await prisma.value.update({
             where: { id: safeBody.data.rowId, listId: safeBody.data.listId },
             data: {
@@ -52,10 +84,13 @@ export async function PUT(req: NextRequest) {
         );
     } catch (err) {
         console.error(err);
-        return NextResponse.json({
-            success: false,
-            message: "Something went wrong",
-            error: err,
-        });
+        return NextResponse.json(
+            {
+                success: false,
+                message: "Something went wrong",
+                error: err,
+            },
+            { status: 500 },
+        );
     }
 }
